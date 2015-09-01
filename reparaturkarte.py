b3a8e733bfe7b5e6d432b23127b9af3eddc4983f
olf <olf@subsignal.org>
2015-09-01 17:54:31 +0200

    added new adminmap page for easier address search

--- reparaturkarte.py
+++ reparaturkarte.py
@@ -2,6 +2,8 @@
 
 import cherrypy
 import sqlite3
+import urllib.request
+import json
 
 from geojson import Feature, Point, FeatureCollection, dumps
 
@@ -44,6 +46,9 @@ categories = { 0 : "Technik",
                 4 : "Sonstiges",
                 5 : "Auto",
                 6 : "Schuhe"}
+
+#nominatim url for address -> coordinates matching
+nominatim_url = "http://nominatim.openstreetmap.org/search?format=json&q="
 
 class Places(object):
 
@@ -97,9 +102,11 @@ class Places(object):
 
             #Create GeoJSON-Feature for each place
             for element in db_extract:
-                desc = "<b>%s</b><br />%s<br /><a href='%s' target='_blank'>Website</a>" % (str(element[1]),
-                                                                            str(element[2]),
-                                                                            str(element[3]))
+                desc = """<b>%s</b><br />
+                        %s<br />
+                        <a href='%s' target='_blank'>Website</a>""" % (str(element[1]),
+                                                                     str(element[2]),
+                                                                     str(element[3]))
                 place = Feature(
                     geometry=Point(
                         (float(element[4]),
@@ -178,6 +185,61 @@ class Backend(object):
         return temp_lookup.get_template("admin.html").render(data=places, error=error)
 
     @cherrypy.expose
+    def adminmap(self, **kwargs):
+        if len(kwargs)>0:
+            try:
+                query = kwargs["q"]
+            except:
+                return "Invalid Request <a href='/adminmap'>Zurück</a>"
+            if (query == "enter"):
+                return(kwargs["lon"], kwargs["lat"])
+        return temp_lookup.get_template("adminmap.html").render()
+
+    @cherrypy.expose
+    def admingeojson(self, **kwargs):
+        cherrypy.response.headers['Content-Type'] = 'application/json'
+        places = []
+        if len(kwargs)>0:
+            try:
+                address = kwargs["address"]
+                query = kwargs["q"]
+            except:
+                # No valid request, so we return an empty FC
+                return str(FeatureCollection(places)).encode('utf-8')
+
+            # Make a Nominatim-Request and generate geoJSON-File from it
+            if (query == "search"):
+
+                request_url = nominatim_url + address.replace(' ', '+')
+                request_url = request_url.encode('ascii', 'xmlcharrefreplace').decode('ascii')
+
+                with urllib.request.urlopen(request_url, timeout=5) as response:
+                    nom_response = response.read()
+                coordinates = json.loads(nom_response.decode("utf-8"))
+
+                for element in coordinates:
+                    name = element["display_name"]
+                    lat = float(element["lat"])
+                    lon = float(element["lon"])
+                    desc = """{0}<br/>
+                            <b>Lon:</b>&nbsp;{1}<br/>
+                            <b>Lat:</b>&nbsp;{2}""".format(name, lon, lat)
+                    #desc = """{0}<br/>
+                    #        <b>Lon:</b>&nbsp;{1}<br/>
+                    #        <b>Lat:</b>&nbsp;{2}<br/>
+                    #        <a href='/adminenter/q=enter&lon={1}&lat={2}'>
+                    #        Eintragen</a>""".format(name, lon, lat)
+                    place = Feature(geometry=Point((lon, lat)),
+                                             properties={
+                                                    "name": coordinates.index(element),
+                                                    "popupContent" : desc
+                                                    })
+                    places.append(place)
+
+        # return empty for invalid request, or no matched object, or FC if successful
+        return str(FeatureCollection(places)).encode('ascii', 'xmlcharrefreplace')
+
+    @cherrypy.expose
     def karte(object):
         return temp_lookup.get_template("karte.html").render()
 
@@ -222,7 +284,13 @@ if __name__ == "__main__":
             "tools.digest_auth.on": True,
             "tools.digest_auth.realm": 'localhost',
             "tools.digest_auth.users": credentials.users
-
+        },
+        '/adminmap': {
+            "tools.staticdir.dir" : os.path.join(current_dir, 'public'),
+            "tools.staticdir.on" : True,
+            "tools.digest_auth.on": True,
+            "tools.digest_auth.realm": 'localhost',
+            "tools.digest_auth.users": credentials.users
         }
     })
 
